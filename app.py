6b16164a86781e5bd0d4502e6226f1cc05ad7b54
Lamin Muhammed M <laminmuhammed@gmail.com>
2023-04-30 22:41:10 +0530

    redirect created

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ whatsapp_account = {
 
 @app.route('/')
 def index():
-    return 'Welcome to Namma Yatri.'
+    return redirect(f"https://api.whatsapp.com/send?phone={whatsapp_account['FROM_PHONE_NUMBER']}&text=Hi")
 
 
 def unique_order_id():
